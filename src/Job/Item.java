d10613076d35d7dd62d70a0e5bab1469d57792b9
Kacper Kielak <kielak.kacper@gmail.com>
2017-03-02 01:17:37 +0000

    comments1

--- src/Job/Item.java
+++ src/Job/Item.java
@@ -4,7 +4,8 @@ import java.awt.Point;
 
 public class Item {
 	
-	private final char code;
+	private final char code; //for sure code has only one character? I know that on slides it has, 
+													 //but if you are not 100% sure I would use String instead of char.
 	private final float weight;
 	private final float reward;
 	private Point position;
@@ -16,7 +17,11 @@ public class Item {
 		this.position = new Point();
 	}
 	
-	public void setPosition(int x, int y){
+	public void setPosition(int x, int y){ //is position of item changing during simulation? 
+																				 //if not it should be moved to constructor 
+																				 //either way it shouldn't take x and y as parameters but Point object
+																				 //ok, if x and y as parameters would be really convinient then ok
+																				 //but still there should be method just with Point object as parameter
 		this.position.x = x;
 		this.position.y = y;
 	}
@@ -25,11 +30,11 @@ public class Item {
 		return position;
 	}
 	
-	public int getPositionX(){
+	public int getPositionX(){ //We have getPosition() method already, I don't think we need this one.
 		return position.x;
 	}
 	
-	public int getPositionY(){
+	public int getPositionY(){ //We have getPosition() method already, I don't think we need this one.
 		return position.y;
 	}
 	
@@ -40,4 +45,8 @@ public class Item {
 	public float getReward(){
 		return reward;
 	}
+
+	//add hashCode() method (because this class is used as a key in hash table inside of Order class)
+
+	//add equals() methods (because this class is used as a key in hash table inside of Order class)
 }
--- src/Job/Order.java
+++ src/Job/Order.java
@@ -5,13 +5,15 @@ import java.util.concurrent.ConcurrentMap;
 
 public class Order {
 	
+	//add order ID?
 	private ConcurrentMap<Item, Integer> orderTable;
 	
 	public Order(int _id){
 		orderTable = new ConcurrentHashMap<Item, Integer>();
 	}
 	
-	public void addItem(Item j, int q){
+	public void addItem(Item j, int q){ //can items be added to orders during simulation? 
+																			//if no then move it to constructor
 		orderTable.put(j, q);
 	}
 	
