64e77c1828de3256c06c6582f622836856f296fc
Kacper Kielak <kielak.kacper@gmail.com>
2017-03-08 13:27:55 +0000

    main method added

---
+++ src-pc/com/bestroboticsteam/main/Main.java
@@ -0,0 +1,32 @@
+package com.bestroboticsteam.main;
+
+import java.awt.Point;
+
+import com.bestroboticsteam.jobs.JobSelection;
+import com.bestroboticsteam.pathfinding.AStar;
+import com.bestroboticsteam.jobs.JobAssignment;
+import com.bestroboticsteam.robotsmanagement.RobotInfo;
+import com.bestroboticsteam.robotsmanagement.RobotsManager;
+import com.bestroboticsteam.warehouseinterface.InterfaceController;
+
+public class Main {
+
+	private static final String PATH = "../assets";
+	private static final RobotInfo[] robots = { 
+			new RobotInfo("robot1", new Point(0, 0))
+		};
+	
+	
+	public static void main(String[] args) {
+		JobSelection jobsSelect = new JobSelection(PATH);
+		JobAssignment jobsAssign = new JobAssignment(jobsSelect);
+		AStar pathFinder = new AStar();
+
+		RobotsManager manager = new RobotsManager(robots, jobsAssign, pathFinder);
+		manager.run();
+		
+		InterfaceController warehouseInterface = new InterfaceController(jobsSelect);
+		warehouseInterface.run();
+	}
+
+}
