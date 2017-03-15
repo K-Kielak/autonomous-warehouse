81986cf189982bbde795d01cca7202d557464567
Alexander Lloyd <alex@alexlloyd.xyz>
2017-03-15 12:07:15 +0000

    Changed Robot Name

--- src-shared/com/bestroboticsteam/communication/RobotNames.java
+++ src-shared/com/bestroboticsteam/communication/RobotNames.java
@@ -2,8 +2,13 @@ package com.bestroboticsteam.communication;
 
 public class RobotNames {
 	public static final String ROBOT_1_NAME = "John Cena";
+<<<<<<< Updated upstream
 	public static final String ROBOT_2_NAME = "Robot2"; 
 	public static final String ROBOT_3_NAME = "";// Not know yet...
 	
 	public static final String[] ROBOT_NAMES = {ROBOT_1_NAME, ROBOT_2_NAME, ROBOT_3_NAME};
+=======
+	public static final String ROBOT_2_NAME = "Hulk Hogan"; 
+	public static final String ROBOT_3_NAME = "Dwayne Johnson";
+>>>>>>> Stashed changes
 }
