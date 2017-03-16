74a993d87e333876c46a09ab24bf814b42d9fa0c
Kacper Kielak <kielak.kacper@gmail.com>
2017-03-16 17:14:18 +0000

    Robot checks for cancelled jobs and maxCapacity added to robot info

--- src-pc/com/bestroboticsteam/robotsmanagement/RobotsManager.java
+++ src-pc/com/bestroboticsteam/robotsmanagement/RobotsManager.java
@@ -36,15 +36,12 @@ public class RobotsManager extends Thread{
 		
 		while(true){
 			for (int i = 0; i < robots.length; i++){
-				if(robots[i].getInfo().finished()){
-					JobInfo job = jobs.getNextJob();
-					RobotInfo currRobotInfo = robots[i].getInfo();
-					RobotInfo[] otherRobotsInfos = getOtherRobotsInfos(currRobotInfo);
-					Point start = currRobotInfo.getPosition();
-					Point goal = job.getPosition();
-					LinkedList<Point> path = AStar.multiGetPath(Pair.makePair(start, goal), otherRobotsInfos);
-					robots[i].assignNewJob(job, path);
-				}
+				RobotInfo robotInfo = robots[i].getInfo();
+				if(!jobs.isCurrentJob(robotInfo.getCurrentJob().getJobCode()))
+					robotInfo.cancelJob();
+				
+				if(robots[i].getInfo().finished())
+					assignNewJobTo(robots[i]);
 			}
 			
 			try {
@@ -61,6 +58,16 @@ public class RobotsManager extends Thread{
 			robotInfos[i] = robots[i].getInfo();
 		
 		return robotInfos;
+	}
+	
+	private void assignNewJobTo(Robot robot){
+		JobInfo job = jobs.getNextJob();
+		RobotInfo currRobotInfo = robot.getInfo();
+		RobotInfo[] otherRobotsInfos = getOtherRobotsInfos(currRobotInfo);
+		Point start = currRobotInfo.getPosition();
+		Point goal = job.getPosition();
+		LinkedList<Point> path = AStar.multiGetPath(Pair.makePair(start, goal), otherRobotsInfos);
+		robot.assignNewJob(job, path);
 	}
 	
 	private RobotInfo[] getOtherRobotsInfos(RobotInfo robotInfo){
