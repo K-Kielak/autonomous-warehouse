e82b19880a0993ef9a4b79e8fbe891b9311b83b7
Alexander Lloyd <alex@alexlloyd.xyz>
2017-03-17 16:06:16 +0000

    Begain interface integration + some modifications to interface

--- src-robot/com/bestroboticsteam/robotexecution/Movement.java
+++ src-robot/com/bestroboticsteam/robotexecution/Movement.java
@@ -14,62 +14,62 @@ public class Movement {
 	private final LightSensor leftSensor;
 	private final LightSensor rightSensor;
 	private final DifferentialPilot pilot;
-	
-	public Movement(LightSensor leftSensor, LightSensor rightSensor, DifferentialPilot pilot){
+
+	public Movement(LightSensor leftSensor, LightSensor rightSensor, DifferentialPilot pilot) {
 		this.CALIBRATED_VALUE = getCalibratedValue(leftSensor, rightSensor);
 		this.leftSensor = leftSensor;
 		this.rightSensor = rightSensor;
 		this.pilot = pilot;
-		
+
 		this.pilot.setTravelSpeed(SPEED);
 	}
-	
-	public void move(Direction direction){
-		switch(direction){
-			case LEFT:
-				pilot.rotate(90);
-				break;
-			case RIGHT:
-				pilot.rotate(-90);
-				break;
-			case BACKWARD:
-				pilot.rotate(180);
-				break;
-			case FORWARD:
-				//dont't rotate
-				break;
+
+	public void move(Direction direction) {
+		switch (direction) {
+		case LEFT:
+			pilot.rotate(90);
+			break;
+		case RIGHT:
+			pilot.rotate(-90);
+			break;
+		case BACKWARD:
+			pilot.rotate(180);
+			break;
+		case FORWARD:
+			// dont't rotate
+			break;
 		}
-		
+
 		boolean isLeftOnBlack = isOnBlack(leftSensor.readValue());
 		boolean isRightOnBlack = isOnBlack(rightSensor.readValue());
-		while(!(isRightOnBlack && isLeftOnBlack)){
+		while (!(isRightOnBlack && isLeftOnBlack)) {
 			pilot.forward();
-			
-			while(isRightOnBlack && !isLeftOnBlack){
+
+			while (isRightOnBlack && !isLeftOnBlack) {
 				pilot.rotateRight();
-				
+
 				isLeftOnBlack = isOnBlack(leftSensor.readValue());
 				isRightOnBlack = isOnBlack(rightSensor.readValue());
 			}
-			
-			while(!isRightOnBlack && isLeftOnBlack){
+
+			while (!isRightOnBlack && isLeftOnBlack) {
 				pilot.rotateLeft();
-				
+
 				isLeftOnBlack = isOnBlack(leftSensor.readValue());
 				isRightOnBlack = isOnBlack(rightSensor.readValue());
 			}
-			
+
 			Delay.msDelay(DELAY);
 			isLeftOnBlack = isOnBlack(leftSensor.readValue());
 			isRightOnBlack = isOnBlack(rightSensor.readValue());
 		}
-		
+
 		pilot.stop();
 		pilot.travel(0.07);
 		pilot.stop();
 	}
-	
-	private int getCalibratedValue(LightSensor leftSensor, LightSensor rightSensor){
+
+	private int getCalibratedValue(LightSensor leftSensor, LightSensor rightSensor) {
 		Delay.msDelay(300);
 		int leftValue = leftSensor.readValue();
 		int rightValue = rightSensor.readValue();
@@ -77,8 +77,8 @@ public class Movement {
 		int calibratedValue = (leftValue + rightValue) / 2;
 		return calibratedValue;
 	}
-	
-	private boolean isOnBlack(int sensorValue){
+
+	private boolean isOnBlack(int sensorValue) {
 		return Math.abs(CALIBRATED_VALUE - sensorValue) > ERROR;
 	}
 }
--- src-robot/com/bestroboticsteam/robotexecution/RobotInterface.java
+++ src-robot/com/bestroboticsteam/robotexecution/RobotInterface.java
@@ -1,112 +1,112 @@
 package com.bestroboticsteam.robotexecution;
-import lejos.nxt.Sound;
 
-
-
-import com.bestroboticsteam.robotsmanagement.Direction;
-import com.bestroboticsteam.robotsmanagement.RobotInfo;
-import java.util.LinkedList;
-
-import javax.microedition.io.Connection;
-
-//import java.util.Optional;
-import com.bestroboticsteam.jobs.JobInfo;
 import java.awt.Point;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+
+import com.bestroboticsteam.communication.RobotCommunicationHandler;
+import com.bestroboticsteam.jobs.JobInfo;
+import com.bestroboticsteam.robotsmanagement.RobotInfo;
+
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.util.Delay;
-import com.bestroboticsteam.communication.*;
 
-public class RobotInterface{
+public class RobotInterface {
 
-	/*private String Confirm_pickup = "Confirm the pick up location";
-	private String Confirm_drop = "Confirm the drop off location";*/
 	private int itemsQuantity = 0;
-	
-	public void setItemsQuantity(int q){
+
+	public void setItemsQuantity(int q) {
 		itemsQuantity = q;
 	}
-	
-	public int getItemsQuantity(){
+
+	public int getItemsQuantity() {
 		return itemsQuantity;
 	}
-			
-	public void bluetoothMessage(String name){
+
+	public void bluetoothMessage(RobotInfo robot, RobotCommunicationHandler comms) {
 		LCD.clear();
-		LCD.drawString(name, 1, 0);
-		LCD.drawString("Waiting for bluetooth connection", 2, 1);
+		LCD.drawString("", 1, 0);
+		LCD.drawString("Status:", 2, 1);
+		LCD.drawString(comms.getStatus(), 4, 2);
 		Delay.msDelay(800);
 	}
-	
-	public void printMovingToItemMessage(String name,int jobCode, String itemCode,Point position){
+
+	public void printMovingToItemMessage(RobotInfo robot) {
 		LCD.clear();
-		LCD.drawString("Robot name: " + name, 1, 0);
-		LCD.drawString("Job code: " + jobCode, 1, 1);
-		LCD.drawString("Item code: " + itemCode, 1, 2);
-		LCD.drawString("X position: " +  position.x + " Y position: " + position.y,1,3);
-	}
-	
-	public void printLoadMessage(String name,String itemCode, int quantity, int number){
-		LCD.clear();
-		LCD.drawString(name, 1, 0);
-		LCD.drawString("Please load: " + quantity + " items " + itemCode + " into the robot",1,1);
-		LCD.drawString("There are: " + number + " items in the robot at the moment", 1, 2);
-	}
-	
-	public void printMovingToDropPointMessage(String name,int jobCode,Point position){
-		LCD.clear();
-		LCD.drawString(name, 1, 0);
-		LCD.drawString("Job code: " + jobCode, 1, 1);
-		LCD.drawString("Destination: " + position.x + ", " + position.y, 1, 2);
-		LCD.drawString("Moving to delivery point...", 1, 3);
+		LCD.drawString("Robot name: ", 1, 0);
+		LCD.drawString("  " + robot.getName() , 1, 1);
+		LCD.drawString("Job code: ", 1, 2);
+		JobInfo j = robot.getCurrentJob();
+		LCD.drawInt(j.getJobCode(), 1, 3);
+		LCD.drawString("Item code: " + j.getItem(), 1, 4);
+		LCD.drawString("Position: ", 1, 5);
+		Point p = robot.getPosition();
+		LCD.drawString("    x: " + p.x, 1,6);
+		LCD.drawString("    y: " + p.y, 1,7);
 		
 	}
-	
-	 private void addItems(){
-			Delay.msDelay(100);
-			itemsQuantity++;
-			LCD.clear();
-			LCD.drawString("Curent number of items is: " , 1, 0);
-			LCD.drawInt(itemsQuantity, 2, 1);
+
+	public void printLoadMessage(RobotInfo robot) {
+		LCD.clear();
+		LCD.drawString(robot.getName(), 1, 0);
+		JobInfo j = robot.getCurrentJob();
+		LCD.drawString("Please load: " + j.getQuantity() + " items " + j.getItem() + " into the robot", 1, 1);
+		LCD.drawString("There are: " + this.itemsQuantity + " items in the robot at the moment", 1, 2);
+	}
+
+	//public void printMovingToDropPointMessage(String name, int jobCode, Point position) {
+	public void printMovingToDropPointMessage(RobotInfo robot) {
+		LCD.clear();
+		LCD.drawString(robot.getName(), 1, 0);
+		LCD.drawString("Job code: " + robot.getCurrentJob().getItem(), 1, 1);
+		Point p = robot.getCurrentJob().getPosition();
+		LCD.drawString("Destination: " + p.x + ", " + p.y, 1, 2);
+		LCD.drawString("Moving to delivery point...", 1, 3);
+
+	}
+
+	private void addItems() {
+		Delay.msDelay(100);
+		itemsQuantity++;
+		LCD.clear();
+		LCD.drawString("Curent number of items is: ", 1, 0);
+		LCD.drawInt(itemsQuantity, 2, 1);
+	}
+
+	public void dropItems() {
+		Delay.msDelay(100);
+		itemsQuantity--;
+		LCD.clear();
+		LCD.drawString("Curent number of items is: ", 1, 0);
+		LCD.drawInt(itemsQuantity, 2, 1);
 	}
 	
-	 public void dropItems(){
-			Delay.msDelay(100);
-			itemsQuantity--;
-			LCD.clear();
-			LCD.drawString("Curent number of items is: " , 1, 0);
-			LCD.drawInt(itemsQuantity, 2, 1);		
-	}	
-	 
-	 
-	 
-	 public void waitForButton(){
-		 int pressedButtonID = Button.waitForAnyPress();
-		 if(pressedButtonID == Button.ID_LEFT)
-			 dropItems();
-		 else if(pressedButtonID == Button.ID_RIGHT)
-			 addItems();
-		 else if(pressedButtonID == Button.ID_ESCAPE){
-			 LCD.drawString("Are you sure that you want to cancel?",1,0);
-			 LCD.drawString("Press RIGHT arrow for \"YES\" or LEFT arrow for \"NO\"",1,1);
-			 int pressedButtonID2 = Button.waitForAnyPress();
-			 if(pressedButtonID2 == Button.ID_RIGHT){
-				 //cancel job
-			 }
-			 else 
-				 waitForButton();
-		 }
-	 }
-	 
-	public void status(int number){
+	public void waitForSensorCalibration() {
 		LCD.clear();
-		LCD.drawString("Curent number of items is: " + number , 1, 0);
+		LCD.drawString("Press Any Button", 1, 0);
+		LCD.drawString("to Calibrate", 1, 1);
+		LCD.drawString("Sensors", 1, 2);
+		Button.waitForAnyPress();				
+	}
+
+	public void waitForButton() {
+		int pressedButtonID = Button.waitForAnyPress();
+		if (pressedButtonID == Button.ID_LEFT)
+			dropItems();
+		else if (pressedButtonID == Button.ID_RIGHT)
+			addItems();
+		else if (pressedButtonID == Button.ID_ESCAPE) {
+			LCD.drawString("Are you sure that you want to cancel?", 1, 0);
+			LCD.drawString("Press RIGHT arrow for \"YES\" or LEFT arrow for \"NO\"", 1, 1);
+			int pressedButtonID2 = Button.waitForAnyPress();
+			if (pressedButtonID2 == Button.ID_RIGHT) {
+				// cancel job
+			} else
+				waitForButton();
+		}
+	}
+
+	public void status(int number) {
+		LCD.clear();
+		LCD.drawString("Curent number of items is: " + number, 1, 0);
 	}
 }
-
-
-	
-
