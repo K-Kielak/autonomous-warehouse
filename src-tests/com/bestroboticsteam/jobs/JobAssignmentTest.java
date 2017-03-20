78ad4e10498dc043298acef7cd33dde8619ab242
Vikov <mvicovan@yahoo.com>
2017-03-20 13:50:13 +0000

    found a bugg in assignment - fixed! (number of jobs assigned to a robot)

--- src-tests/com/bestroboticsteam/jobs/JobAssignmentTest.java
+++ src-tests/com/bestroboticsteam/jobs/JobAssignmentTest.java
@@ -14,20 +14,13 @@ public class JobAssignmentTest {
 	public static void ReadData(){
 		
 		selector = new JobSelection("../central-system/assets/test");
-		assignment = new JobAssignment(selector);
+		
 	
 	}
 	
 	@Test
 	public void TestNextJob(){
 		
-		JobInfo info = assignment.getNextJob();
-		
-		assertEquals(info.getItem(), "a");
-		assertEquals(info.getJobCode(), 1002);
-		assertEquals(info.getQuantity(), 5);
-		assertEquals(info.getPosition().x, 1);
-		assertEquals(info.getPosition().y, 1);
 		
 	}
 	
