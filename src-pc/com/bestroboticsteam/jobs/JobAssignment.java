8659ce37da7fec0e12335061eabf8fd967a60839
Vikov <mvicovan@yahoo.com>
2017-03-20 13:29:36 +0000

    assignment based on distance with A*

--- src-pc/com/bestroboticsteam/jobs/JobAssignment.java
+++ src-pc/com/bestroboticsteam/jobs/JobAssignment.java
@@ -339,7 +339,7 @@ public class JobAssignment extends Thread {
 	
 	private int averageDistance(Point point, Point point2) {
 		
-		return Math.abs(point.x - point2.x) + Math.abs(point.y - point2.y);
+		return AStar.singleGetPath(point, point2).size();
 	}
 	
 	private Point getDrop(JobInfo info){
