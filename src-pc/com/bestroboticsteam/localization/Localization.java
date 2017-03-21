5723ae6c158645313bbcc35ea6e1de0272fe842e
ldw606 <ldw606@student.bham.ac.uk>
2017-03-21 11:36:45 +0000

    Fixed typos and added basic code

--- src-pc/com/bestroboticsteam/localization/Localization.java
+++ src-pc/com/bestroboticsteam/localization/Localization.java
@@ -2,56 +2,90 @@ package com.bestroboticsteam.localization;
 
 import java.awt.Point;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.ParagraphAction;
-
 import com.bestroboticsteam.robotsmanagement.Direction;
 
-import lejos.nxt.I2CPort;
-import lejos.nxt.I2CSensor;
-import lejos.nxt.SensorPort;
-import lejos.nxt.UltrasonicSensor;
-import lejos.robotics.RangeFinder;
-import lejos.robotics.navigation.Pose;
 import rp.robotics.mapping.GridMap;
 import rp.robotics.mapping.MapUtils;
-import rp.util.Pair;
 
 public class Localization {
 
-	private double[][][] poseLikelyhoods;
+	private double[][][] poseLikelihoods;
 	private GridMap map;
 	Direction nextMovement;
 
 	public Localization() {
 		map = MapUtils.createRealWarehouse();
+		// Initialise all poses as equally likely
+		for (int i = 0; i < poseLikelihoods.length; i++) {
+			for (int j = 0; j < poseLikelihoods[i].length; j++) {
+				for (int k = 0; k < poseLikelihoods[i].length; k++) {
+					poseLikelihoods[i][j][k] = 1 / (poseLikelihoods.length * poseLikelihoods[0].length);
+				}
+			}
+		}
 	}
 
-	
-	public void step(float range){
-		
+	public void step(float range) {
+		switch (nextMovement) {
+		case FORWARD:
+			for (int i = 0; i < poseLikelihoods.length - 1; i++) {
+				// Move all likelihoods upwards
+				poseLikelihoods[i] = poseLikelihoods[i + 1];
+			}
+			break;
+		case RIGHT:
+			for (int i = 0; i < poseLikelihoods.length; i++) {
+				for (int j = poseLikelihoods.length - 1; j > 0; j--) {
+					// Move all likelihoods to the right
+					poseLikelihoods[i][j] = poseLikelihoods[i][j - 1];
+				}
+			}
+			break;
+		case BACKWARD:
+			for (int i = poseLikelihoods.length - 1; i > 0; i--) {
+				// Move all likelihoods downwards
+				poseLikelihoods[i] = poseLikelihoods[i - 1];
+			}
+			break;
+		case LEFT:
+			for (int i = 0; i < poseLikelihoods.length; i++) {
+				for (int j = 0; j < poseLikelihoods.length - 1; j++) {
+					// Move all likelihoods to the left
+					poseLikelihoods[i][j] = poseLikelihoods[i][j + 1];
+				}
+			}
+			break;
+		default: // null
+
+			break;
+		}
 		nextMovement = Direction.FORWARD;
 	}
-	
-	public PoseLikelyhood mostLikelyPose() {
-		double maxLikelyhood = 0;
-		PoseLikelyhood mostLikelyPose = null;
-		for (int i = 0; i < poseLikelyhoods.length; i++) {
-			for (int j = 0; j < poseLikelyhoods[i].length; j++) {
-				for (int k = 0; k < poseLikelyhoods[i].length; k++) {
-					if (poseLikelyhoods[i][j][k] > maxLikelyhood) {
-						maxLikelyhood = poseLikelyhoods[i][j][k];
+
+	public PoseLikelihood mostLikelyPose() {
+		double maxLikelihood = 0;
+		PoseLikelihood mostLikelyPose = null;
+		for (int i = 0; i < poseLikelihoods.length; i++) {
+			for (int j = 0; j < poseLikelihoods[i].length; j++) {
+				for (int k = 0; k < poseLikelihoods[i].length; k++) {
+					if (poseLikelihoods[i][j][k] > maxLikelihood) {
+						maxLikelihood = poseLikelihoods[i][j][k];
 						switch (k) {
 						case 0:
-							mostLikelyPose = new PoseLikelyhood(poseLikelyhoods[i][j][k], new Point(i, j), Direction.FORWARD);
+							mostLikelyPose = new PoseLikelihood(poseLikelihoods[i][j][k], new Point(i, j),
+									Direction.FORWARD);
 							break;
 						case 1:
-							mostLikelyPose = new PoseLikelyhood(poseLikelyhoods[i][j][k], new Point(i, j), Direction.RIGHT);
+							mostLikelyPose = new PoseLikelihood(poseLikelihoods[i][j][k], new Point(i, j),
+									Direction.RIGHT);
 							break;
 						case 2:
-							mostLikelyPose = new PoseLikelyhood(poseLikelyhoods[i][j][k], new Point(i, j), Direction.BACKWARD);
+							mostLikelyPose = new PoseLikelihood(poseLikelihoods[i][j][k], new Point(i, j),
+									Direction.BACKWARD);
 							break;
 						default:
-							mostLikelyPose = new PoseLikelyhood(poseLikelyhoods[i][j][k], new Point(i, j), Direction.LEFT);
+							mostLikelyPose = new PoseLikelihood(poseLikelihoods[i][j][k], new Point(i, j),
+									Direction.LEFT);
 							break;
 						}
 					}
@@ -63,18 +97,18 @@ public class Localization {
 
 	private void normalizeLocations() {
 		double sum = 0;
-		for (double[][] row : poseLikelyhoods) {
+		for (double[][] row : poseLikelihoods) {
 			for (double[] position : row) {
-				for (double poseLikelyhood : position) {
-					sum += poseLikelyhood;
+				for (double poseLikelihood : position) {
+					sum += poseLikelihood;
 				}
 			}
 		}
-		double normalizationFactor = sum / (poseLikelyhoods.length * poseLikelyhoods[0].length);
-		for (int i = 0; i < poseLikelyhoods.length; i++) {
-			for (int j = 0; j < poseLikelyhoods[i].length; j++) {
-				for (int k = 0; k < poseLikelyhoods[i].length; k++) {
-					poseLikelyhoods[i][j][k] = poseLikelyhoods[i][j][k] / normalizationFactor;
+		double normalizationFactor = sum / (poseLikelihoods.length * poseLikelihoods[0].length);
+		for (int i = 0; i < poseLikelihoods.length; i++) {
+			for (int j = 0; j < poseLikelihoods[i].length; j++) {
+				for (int k = 0; k < poseLikelihoods[i].length; k++) {
+					poseLikelihoods[i][j][k] = poseLikelihoods[i][j][k] / normalizationFactor;
 				}
 			}
 		}
--- src-pc/com/bestroboticsteam/localization/PoseLikelihood.java
+++ src-pc/com/bestroboticsteam/localization/PoseLikelihood.java
@@ -4,13 +4,13 @@ import java.awt.Point;
 
 import com.bestroboticsteam.robotsmanagement.Direction;
 
-public class PoseLikelyhood {
+public class PoseLikelihood {
 
 	public final double likelyhood;
 	public final Point location;
 	public final Direction direction;
 	
-	public PoseLikelyhood(double likelyhood, Point location, Direction direction) {
+	public PoseLikelihood(double likelyhood, Point location, Direction direction) {
 		this.likelyhood = likelyhood;
 		this.location = location;
 		this.direction = direction;
