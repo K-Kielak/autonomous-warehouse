13cb03b4eba4f8172267e5e0ebc3090ab03ee5c4
Alexander Lloyd <alex@alexlloyd.xyz>
2017-03-23 20:41:53 +0000

    Change robot name

--- src-shared/com/bestroboticsteam/communication/RobotNames.java
+++ src-shared/com/bestroboticsteam/communication/RobotNames.java
@@ -6,7 +6,7 @@ public class RobotNames {
 	 * 
 	 */
 	public static final String ROBOT_1_NAME = "John Cena";
-	public static final String ROBOT_2_NAME = "Hulk Hogan"; 
-	public static final String ROBOT_3_NAME = "Teal'c";
+	public static final String ROBOT_2_NAME = "Teal'c"; 
+	public static final String ROBOT_3_NAME = "Dwayne Johnson";
 	public static final String[] ROBOT_NAMES = {ROBOT_1_NAME, ROBOT_2_NAME, ROBOT_3_NAME};
 }
