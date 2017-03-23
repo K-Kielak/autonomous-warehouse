94b5122062e2c1d52970f7c960fefb97a0e52b8d
Alexander Lloyd <alex@alexlloyd.xyz>
2017-03-23 19:38:48 +0000

    Added comments to communication section

--- src-shared/com/bestroboticsteam/communication/RobotNames.java
+++ src-shared/com/bestroboticsteam/communication/RobotNames.java
@@ -1,6 +1,10 @@
 package com.bestroboticsteam.communication;
 
 public class RobotNames {
+	/**
+	 * Names of the robots in a useful place
+	 * 
+	 */
 	public static final String ROBOT_1_NAME = "John Cena";
 	public static final String ROBOT_2_NAME = "Hulk Hogan"; 
 	public static final String ROBOT_3_NAME = "Dwayne Johnson";
