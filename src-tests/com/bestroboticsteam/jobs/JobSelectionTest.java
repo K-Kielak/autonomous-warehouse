e7740a8a35f6ced9b2ccd3b7a6a8368261e7fb49
Vikov <mvicovan@yahoo.com>
2017-03-24 06:35:08 +0000

    back to the selection

--- src-tests/com/bestroboticsteam/jobs/JobSelectionTest.java
+++ src-tests/com/bestroboticsteam/jobs/JobSelectionTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import org.junit.BeforeClass;
@@ -13,15 +14,23 @@ public class JobSelectionTest {
 	
 
 	private static JobSelection selector;
+	private final int numb = 10000;
 
 	@BeforeClass
 	public static void ReadData(){
 		
-		selector = new JobSelection("../central-system/assets/test");
+		selector = new JobSelection("../central-system/assets/test/TEST1");
 	}
 	
 	@Test
-	public void TestReward(){
+	public void TestPrediction1(){
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		values.add(numb+4);
+		values.add(numb+8);
+		values.add(numb+22);
+		values.add(numb);
+		values.add(numb);
+		
 		
 	
 	}
